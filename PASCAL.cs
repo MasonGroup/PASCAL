8db61a34a77c86e1badf6bab39c5e7b9597d939b
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-09-19 13:07:36 +0300

    Add files via upload

---
+++ PASCAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        MessageBox.Show("Gangsta freemasonry made me do it", "MasonRootkit");
+        Environment.Exit(0);
+    }
+}
